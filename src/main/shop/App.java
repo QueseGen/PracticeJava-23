9880e94e7477b747554f9e352c3d35fd900fe7d9
QueseGen <jacquesewilson@gmail.com>
2023-02-08 10:52:44 -0500

    testing create and read ops

--- src/main/shop/App.java
+++ src/main/shop/App.java
@@ -7,8 +7,15 @@ import shop.model.Product;
 public class App {
     
     public static void main(String[] args) {
-      Product setinventory= new Product();
-      
+     //Not sure if relevant but Collection
+      Product seeinventory= new Product();
+    
+      new Product("Nike slides",34.99,8, "blue/white");
+      new Product("Nike slidez",34.99,8, "black/white");
+      new Product("Nike shoes",60.99,16, "white");
+      new Product("Nike shoes", 60.99, 16,  "black");
+      new Product("Benzzys",34.99,8, "black/white");
+      seeinventory.getInventory("Benzzys");
       
     
     }
