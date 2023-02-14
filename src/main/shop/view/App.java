18daef748adff3d039f6c8d2f1f353ef5ee8a353
QueseGen <jacquesewilson@gmail.com>
2023-02-14 12:52:34 -0500

    added another validat

--- src/main/shop/view/App.java
+++ src/main/shop/view/App.java
@@ -11,7 +11,7 @@ public class App {
     static boolean sessionEnded=false;
 
     public static void main(String[] args) {
-        // Create Test data
+        
         Warehouse seeinventory = new Warehouse();
         clearscreen();
 
@@ -24,30 +24,30 @@ public class App {
         admin.Ops("add", new Product("Air Jordan", 60.99, 16, "The highly anticipated movie, Air, is set to make its grand premiere on April 5,\t gracing over 3,000 movie theatre screens across the United States. The excitement doesn't end there, as it will soon be available for streaming in over 240 countries, providing a truly global experience for fans everywhere."));
         admin.Ops("add", new Product("Benzzy Airs", 34.99, 8, "black/white"));
        
-        User currentUser= new User(); //User testUser= new User("Elon", "Musky", false);
+        User currentUser= new User(); 
        
         System.out.printf("\n--------------Welcome to PopShop--------------\nPlease Login. \n\n\tUsername: ");
-        Scanner scannerTest = new Scanner(System.in);
+        Scanner userInput = new Scanner(System.in);
         
-        currentUser.setUsername(scannerTest.nextLine()); 
+        currentUser.setUsername(userInput.nextLine()); 
         System.out.printf("\tPassword: ");
-        currentUser.setPassword(scannerTest.nextLine());
+        currentUser.setPassword(userInput.nextLine());
             
         System.out.printf("\n\nGreetings %s, looking to shop today?", currentUser.getUsername());
         Product selected=null;
         currentUser.openShoppingCart();
-         String input= scannerTest.nextLine();
+         String input= userInput.nextLine();
         do {
             switch (input) {
                 case "Yes", "Y", "y", "yes", "til I drop", "FlY", "EAGLES":
                     clearscreen();
                     System.out.println("Great! Let's see whats in stock and you can make a selection:");
                     seeinventory.getInventory();
-                    input=scannerTest.nextLine();
+                    input=userInput.nextLine();
                     
                     if (!IsInt(input, Warehouse.IventoryList().size())){ 
                         System.out.print("Enter a Product # for more options: ");
-                        input = scannerTest.nextLine(); } 
+                        input = userInput.nextLine(); } 
                     
                     selected= seeinventory.IventoryList().get(Integer.parseInt(input)-1);
 
@@ -57,7 +57,7 @@ public class App {
                     System.out.printf("Product #%d %15s |\tSelling at $%.2f per pair | %2s %d in stock.\n\n \t\tDescription:\n\t%s", (Integer.parseInt( input)), selected.getProductname(),
                             selected.getProductprice(), "Currently", selected.getProductQuantity(), selected.getProductdescription());
                     System.out.println("\n\n\tWhat would you like to do:\n#1: Add to Cart \n#2: Go back to Menu \n#3: View Shopping Cart");
-                    input = scannerTest.nextLine();
+                    input = userInput.nextLine();
                     break;
 
                 case "No", "N", "n", "nope","not yet", "no", "Exit", "exit", "nathan":
@@ -68,14 +68,14 @@ public class App {
                         clearscreen();
                         if (selected==null){  input="Y";} else{
                             System.out.print("Add how many: ");
-                            input=scannerTest.nextLine();
+                            input=userInput.nextLine();
                             if (IsInt(input)){
                                 currentUser.getPendingOrder().addItem(seeinventory.IventoryList().indexOf(selected)+1, 
                                         Integer.parseInt(input)
                                         );
                                         System.out.println(
                                                 "\nWhats next?\n#1: Add to Cart \n#2: Go back to Menu \n#3: View Shopping Cart \n#4: Checkout");
-                                        input = scannerTest.nextLine();
+                                        input = userInput.nextLine();
                                     } else{ System.out.println("Please try again");
                                         input="1";}
                         }
@@ -90,34 +90,36 @@ public class App {
                         clearscreen();
                         currentUser.getPendingOrder().printItems();
                         System.out.println("\nNice!\n #1: Remove from cart \n #2: Update Item  \n #3: Keep Shopping \n #4: Checkout");
-                        input=scannerTest.nextLine();
+                        input=userInput.nextLine();
 
                         switch(input){
                             case "1":
                                 System.out.println("Remove, Which #?");
-                                input=scannerTest.nextLine();
+                                input=userInput.nextLine();
                                 if (IsInt(input)) {
+                                    if (seeinventory.IventoryList().size()> Integer.parseInt(input)){
                                     selected = seeinventory.IventoryList().get(Integer.parseInt(input)+1);
-                                    currentUser.getPendingOrder().addItem(Integer.parseInt(input),0);
+                                    currentUser.getPendingOrder().addItem(Integer.parseInt(input),0); 
+                                  
                                     System.out.println( "\nWhats next?\n#1: Go back to Menu\n#2: Add Item back to Cart  \n#3: View Shopping Cart \n#4: Checkout");
-                                    input = scannerTest.nextLine();
-                                } else {
+                                    input = userInput.nextLine();
+                                }} else {
                                     System.out.println("Please try again");
                                     input = "3";
                                 }
                                 break;
                             case "2":
                                 System.out.println("Update, Which #?");
-                                input = scannerTest.nextLine();
+                                input = userInput.nextLine();
                                 if (IsInt(input)) {
+                                    if (seeinventory.IventoryList().size()> Integer.parseInt(input)){
                                     selected = seeinventory.IventoryList().get(Integer.parseInt(input));
                                     System.out.println("How many: ");
-                                    String many=scannerTest.nextLine();
+                                    String many=userInput.nextLine();
                                     if(IsInt(many)){
                                         currentUser.getPendingOrder().updateCart(Integer.parseInt(input), Integer.parseInt(many));
                                     System.out.println( "\nWhats next?\n#1: Go back to Menu\n#2: Add more of item to Cart  \n#3: View Shopping Cart \n#4: Checkout");
-                                    input = scannerTest.nextLine();}
-
+                                    input = userInput.nextLine();}}
                                 } else {
                                     System.out.println("Please try again");
                                     input = "3";
@@ -134,7 +136,7 @@ public class App {
                                 clearscreen();
                                 System.out.println(
                                         "I don't understand..please answer Yes, Menu, No, Exit or Shopping Cart:");
-                                input = scannerTest.nextLine();
+                                input = userInput.nextLine();
                                 break;};
                         break;
 
@@ -147,12 +149,12 @@ public class App {
                 default:
                     clearscreen();
                     System.out.println("I don't understand..please answer Yes, Menu, No, Exit or Shopping Cart:");
-                    input = scannerTest.nextLine();                   
+                    input = userInput.nextLine();                   
                      break;
             }
         } while (!sessionEnded);
         System.out.printf("Thanks for shopping %s, Hasta Luego!" ,currentUser.getUsername());
-       scannerTest.close();
+       userInput.close();
     }
 
     private static void clearscreen() {
